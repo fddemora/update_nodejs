be21e20536ef98e57384ba56df573bb4cfa06844
Fellipe Demoraes <demoraes.cyber@gmail.com>
2025-01-18 22:29:25 -0500

    removed node and used bash to compare

--- update_node.sh
+++ update_node.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
 
-cd $(cd "$(dirname "$0")"; pwd -P)
-x=$(node compare_versions.js)
+# Assume that node is installed
+current=$(node -v)
+latest=$(curl -sL https://nodejs.org/dist/latest-v22.x/ | grep -oP 'v\d+\.\d+\.\d+' | uniq)
 
-if [[ $x == '' ]]; then
-    # x is empty. No new version available.
-    echo 'No updates available.'  
-else
-    wget "https://nodejs.org/dist/$x/node-$x-linux-x64.tar.xz"
-    tar -xJf "node-$x-linux-x64.tar.xz"
-    mv "node-$x-linux-x64 /opt/nodejs/"
-    rm "node-$x-linux-x64.tar.xz"
-
-    # update links
-    for i in 'node' 'npm' 'npx'; do
-        if [[ -h "/usr/local/bin/$i" ]]; then
-            rm -f "/usr/local/bin/$i"
-        fi
-
-        # Create a nodejs folder if not present
-        if [[ ! -d '/opt/nodejs' ]]; then
-            mkdir /opt/nodejs
-        fi
-        ln -s "/opt/nodejs/node-$x-linux-x64/bin/$i" "/usr/local/bin/$i"
-    done
-
+if [[ $current == $latest ]]; then
+    exit 1
 fi
+
+wget "https://nodejs.org/dist/${latest}/node-${latest}-linux-x64.tar.xz"
+tar -xJf "node-${latest}-linux-x64.tar.xz"
+
+# Create a nodejs folder if not present
+if [[ ! -d '/opt/nodejs' ]]; then
+    mkdir /opt/nodejs
+fi
+
+mv "node-${latest}-linux-x64 /opt/nodejs/"
+rm "node-${latest}-linux-x64.tar.xz"
+
+# update links
+for i in 'node' 'npm' 'npx'; do
+    if [[ -h "/usr/local/bin/$i" ]]; then
+        rm -f "/usr/local/bin/$i"
+    fi
+    ln -s "/opt/nodejs/node-${latest}-linux-x64/bin/$i" "/usr/local/bin/$i"
+done
+
+exit 0
+
+
 
 
 
